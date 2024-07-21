8b4abdd619f09556f31694f820e496e4ce6ba8cb
Kemal Yılmaz <xkema@tutanota.com>
2024-07-21 23:01:15 +0300

    Created a dummy version to work on animations and styles
    - Added a really simple markup to split main page to components
    - Added export images
    - Added favicon
    - Added textual content as a data file

---
+++ src/data/features.ts
@@ -0,0 +1,43 @@
+const features = [
+  {
+    id: '825cc467',
+    name: 'Document Scanner',
+    title: 'Scan with Ease',
+    description:
+      'Scan any document instantly with your mobile device by just a few steps. Save as PDF,JPG,ZIP,TXT and Word format.',
+    link: '#',
+  },
+  {
+    id: '63023138',
+    name: 'Sign & Stamp',
+    title: 'One-Tap Focus',
+    description:
+      'Draw, scan or import your signature and stamp with a simple touch. Sign and stamp any document with just a single tap!',
+    link: '#',
+  },
+  {
+    id: '6fe926d7',
+    name: 'Batch Scanning',
+    title: 'Multiple Page Scan',
+    description:
+      'Scan multiple pages or documents in multiple-scanning mode. Batch all scans as a single document.',
+    link: '#',
+  },
+  {
+    id: '617df559',
+    name: 'Advanced Filters',
+    title: 'Unique Filters',
+    description:
+      'Apply advanced filters and enhance quality with various custom made filters. Manually edit brightness and contrast by your own choice on the custom filters.',
+    link: '#',
+  },
+  {
+    id: 'c8ce03e8',
+    name: 'Export & Share',
+    title: 'All-Round Conversion',
+    description: 'Export your scans as PDF,JPG,ZIP,TXT and Word.',
+    link: '#',
+  },
+];
+
+export { features };
