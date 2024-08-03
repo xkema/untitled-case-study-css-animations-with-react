f906d1059f3eb1220392e9fbc6181f0e8887aed3
Kemal Yılmaz <xkema@tutanota.com>
2024-08-03 21:39:50 +0300

    Switched Vite to the 'mpa' mode wit 4 routes for demos
    - Created demo directories for each planned demo
    - Added input paths for the 'mpa' mode
    - Added `@node/types`
    - Styles are still shared

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        [`css-with-unmounting`]: resolve(
+          __dirname,
+          'demos/css-with-unmounting/index.html',
+        ),
+        [`css-without-unmounting`]: resolve(
+          __dirname,
+          'demos/css-without-unmounting/index.html',
+        ),
+        [`framer-motion`]: resolve(__dirname, 'demos/framer-motion/index.html'),
+        [`react-transition-group`]: resolve(
+          __dirname,
+          'demos/react-transition-group/index.html',
+        ),
+      },
+    },
+  },
   plugins: [react()],
-})
+  appType: 'mpa',
+});
