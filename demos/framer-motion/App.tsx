d25bb2fd4bdbbd498fb76fe3938041008d8e8eab
Kemal Yılmaz <xkema@tutanota.com>
2024-08-21 10:53:15 +0300

    [WIP] Responsive animation experiment

--- demos/framer-motion/App.tsx
+++ demos/framer-motion/App.tsx
@@ -1,6 +1,16 @@
+import { motion } from 'framer-motion';
 import styles from '../../src/App.module.css';
 
+import aaa from '../../src/assets/images/particles/export-01-arrow-130x156.png';
+import bbb from '../../src/assets/images/particles/export-02-pdf-130x156.png';
+import ccc from '../../src/assets/images/particles/export-03-jpg-130x156.png';
+import ddd from '../../src/assets/images/particles/export-04-txt-130x156.png';
+import { useState } from 'react';
+import { relative } from 'path';
+
 export function App() {
+  const [rotate, setRotate] = useState(!false);
+
   return (
     <>
       <header>
@@ -9,7 +19,36 @@ export function App() {
         <hr />
       </header>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div
+            className={`${styles['moving-element']} ${rotate && styles['moving-element-animation']}`}
+          ></div>
+        </div>
+
+        <div className={styles.container}>
+          <motion.div
+            initial={{
+              offsetPath: `circle(25% at 0% 0%)`,
+              offsetDistance: '25%',
+              offsetRotate: '0deg',
+            }}
+            animate={{
+              offsetPath: `circle(25% at 0% 0%)`,
+              offsetDistance: '0%',
+              offsetRotate: '45deg',
+            }}
+            transition={{ duration: 1, ease: 'linear' }}
+            className={styles.box}
+          >
+            <img src={ccc} alt="ccc" />
+          </motion.div>
+        </div>
+
+        <hr />
+
+        <button onClick={() => setRotate((p) => !p)}>toggle rotate</button>
+      </main>
     </>
   );
 }
