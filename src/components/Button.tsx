f0a95a9b4ffb938eaddb2d9a15014a527d846309
Kemal Yılmaz <xkema@tutanota.com>
2024-08-28 17:54:26 +0300

    Added `.navigation-button` hover and border animations

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,6 +3,7 @@ import styles from '../../src/App.module.css';
 import { ButtonIconSelector } from './ButtonIconSelector';
 import { Feature } from '../data/features';
 import { ButtonIconBackground } from './ButtonIconBackground';
+import { motion } from 'framer-motion';
 
 export type ButtonProps = {
   feature: Feature;
@@ -12,17 +13,28 @@ export type ButtonProps = {
 
 export function Button({ feature, activeId, setActiveId }: ButtonProps) {
   return (
-    <button
+    <motion.button
       className={`${styles['navigation-button']} ${activeId === feature.id ? styles['active'] : ''}`}
       onClick={() => {
         setActiveId(feature.id);
       }}
+      animate={
+        activeId === feature.id
+          ? { backgroundColor: 'var(--background-color-link-active)' }
+          : { backgroundColor: '#ffffff' }
+      }
+      whileHover={
+        activeId !== feature.id
+          ? { borderColor: 'var(--color-border-navigation-button)' }
+          : {}
+      }
+      transition={{ duration: 0.3 }}
     >
       <span className={styles['navigation-button-icon']}>
         <ButtonIconBackground feature={feature} activeId={activeId} />
         <ButtonIconSelector featureId={feature.id} featureName={feature.name} />
       </span>
       <span className={styles['navigation-button-text']}>{feature.name}</span>
-    </button>
+    </motion.button>
   );
 }
