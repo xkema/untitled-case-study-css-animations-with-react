496f467705312009925a97abb9fdec2780d90b75
Kemal Yılmaz <xkema@tutanota.com>
2024-08-30 01:07:05 +0300

    Removed `emptyOutDir` option from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-  },
   plugins: [react()],
   // Replace the `{DEMO_NAME}` with the corresponding demo names; `initial-version`, `abstract-version` or `framer-motion`.
   base: '/untitled-case-study-css-animations-with-react/demos/abstract-version/',
