fc3afcb6b304e8ab7e50ca3e51dc85bcbe997cdc
Kemal Yılmaz <xkema@tutanota.com>
2024-08-30 18:51:57 +0300

    Prepared docs folder index for the demos and GitHub pages deployment
    - Corrected Vite `base` path config option for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Replace the `{DEMO_NAME}` with the corresponding demo names; `initial-version`, `abstract-version` or `framer-motion`.
+  base: '/untitled-case-study-css-animations-with-react/demos/{DEMO_NAME}/',
 })
