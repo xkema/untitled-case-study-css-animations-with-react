eb682b4e71913bc854e4f4a6c9d07a57aceedd58
Kemal Yılmaz <xkema@tutanota.com>
2024-08-31 14:08:35 +0300

    Changed Vite `outDir` option to the demos directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Replace the `{DEMO_NAME}` with the corresponding demo names; `initial-version`, `abstract-version` or `framer-motion`.
-  base: '/untitled-case-study-css-animations-with-react/demos/abstract-version/',
+  base: '/untitled-case-study-css-animations-with-react/demos/framer-motion/',
   build: {
-    outDir: 'docs/demos/abstract-version/'
+    outDir: 'docs/demos/framer-motion/'
   }
 })
